5c55178dc2b6aaffc8f64e1bc96fc36e184902de
Abdulmajid Bakro <abdbakroo@gmail.com>
2024-11-25 19:29:05 +0100

    modify linked service to db

--- src/LinkedService/linked_service_to_db.py
+++ src/LinkedService/linked_service_to_db.py
@@ -1,12 +1,4 @@
 from LinkedService.linked_service import LinkedService
 
 class LinkedServiceToDB(LinkedService):
-    def __init__(self, db_name, user, password, host, port):
-        self.db_name = db_name
-        self.user = user
-        self.password = password
-        self.host = host
-        self.port = port
-
-    def connect(self):
-        pass
+    pass
