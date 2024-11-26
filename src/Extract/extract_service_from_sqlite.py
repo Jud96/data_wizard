b10985501de23d327c45991ffadb4a9244ec93db
Abdulmajid Bakro <abdbakroo@gmail.com>
2024-11-26 08:50:12 +0100

    add extract service from sqlite

---
+++ src/Extract/extract_service_from_sqlite.py
@@ -0,0 +1,15 @@
+from extract_service import ExtractService
+import pandas as pd
+import sqlite3
+
+class ExtractServiceFromsqlite(ExtractService):
+    def __init__(self, db_path, query):
+        self.db_path = db_path
+        self.query = query
+
+    def extract(self):
+        conn = sqlite3.connect(self.db_path)
+        data = pd.read_sql_query(self.query, conn)
+        data['source'] = 'SQL'
+        conn.close()
+        return data
