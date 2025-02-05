dc8fa9dd316fd08f6fd744923afe88689f08d0e8
Abdulmajid Bakro <abdbakroo@gmail.com>
2025-02-05 13:14:58 +0100

    add test from api to db

--- test/intrgration_test/activities/test_copy_activity.py
+++ test/intrgration_test/activities/test_copy_activity.py
@@ -5,6 +5,7 @@ from src.LinkedService.linked_service_postgres import LinkedServicePostgres
 from src.Activity.execute_sql import ExecuteSQL
 from src.Datasets.dataset_from_datalake import DatasetFromDataLake
 from src.Datasets.dataset_from_db import DatasetFromDB
+from src.Datasets.dataset_from_api import DatasetFromAPI
 from src.Activity.copy_activity import CopyActivity
 from dotenv import load_dotenv
 load_dotenv()
@@ -32,8 +33,8 @@ class TestCopyActivity(unittest.TestCase):
         cls.postgres_conn.close()
 
     def test_copy_activity_db_db(self):
-        ExecuteSQL(self.postgres_conn, 'CREATE TABLE IF NOT EXISTS people\
-                    (user_id VARCHAR(100), language_name VARCHAR(100), level VARCHAR(100))').execute()
+        ExecuteSQL(self.postgres_conn).execute( 'CREATE TABLE IF NOT EXISTS people\
+                    (user_id VARCHAR(100), language_name VARCHAR(100), level VARCHAR(100))')
         dataset_source = DatasetFromDB(self.postgres_conn, table_name='people')
         dataset_sink = DatasetFromDB(self.postgres_conn, table_name='people_dim')
         mapping = {'id': 'user_id', 'name': 'language_name', 'level': 'level'}
@@ -76,6 +77,19 @@ class TestCopyActivity(unittest.TestCase):
         response = self.s3_client.list_objects(Bucket=self.bucket, Prefix='people_dim.parquet')
         # check if the file is created
         self.assertIn('Contents', response)
+
+    def test_copy_api_db(self):
+        url = 'http://localhost:8000/api/user_languages'
+        source_api = DatasetFromAPI(url)
+        sink_postgres = DatasetFromDB(self.postgres_conn, table_name='temp')
+        mapping = {'user_id': 'user_id', 'language': 'lang_name', 'level': 'lvl'}
+        copy_activity = CopyActivity(source_api, sink_postgres, mapping)
+        copy_activity.copy()
+        # check if the table is created and data is copied
+        datasource = DatasetFromDB(self.postgres_conn, table_name='temp')
+        data = datasource.extract()
+        # check data size in the table > 0
+        self.assertGreater(data.shape[0], 0)
 
 
 if __name__ == '__main__':
